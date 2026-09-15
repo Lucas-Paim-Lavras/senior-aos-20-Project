8a438b7691bcedb30d41fc0b51109fb73e17126d
Luffyx31613 <miguelmartinsbatista30@gmail.com>
2026-09-15 17:09:57 -0300

    resoluçao ex01

--- avaliacao-logica-js-miguel/src/ex1.js
+++ avaliacao-logica-js-miguel/src/ex1.js
@@ -1,15 +1,15 @@
 let nome = "patrick"
-let nota1 = 1
+let nota1 = 8
 let nota2 = 9
 let media = (nota1 + nota2) /2
-let freq = 80 / 100
+let freq = (80 / 100) * 100 
 let resultado;
-if(freq >= 75 / 100 && media >= 6){
+if(freq >= 75 / 100  && media >=6.0){
     resultado ="aprovado"
-} else if (freq >= 75 / 100 && media >=5){
+} else if (freq >= (75 / 100) * 100 && media >=5.0){
     resultado ="recuperação"
 } else {
     resultado ="reprovado"
 }
 
-console.log(`com a media: ${media}, e ${freq} de frequencia, o aluno: ${nome},esta de: ${resultado}`)
+console.log(`com a media: ${media},e ${freq}% de frequencia,o aluno:${nome},esta:${resultado}`)
