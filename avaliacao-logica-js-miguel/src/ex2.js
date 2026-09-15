f5650ef392b6ebe849efccf82d3b72116bbba1b9
Luffyx31613 <miguelmartinsbatista30@gmail.com>
2026-09-15 17:37:56 -0300

    resolução ex02

---
+++ avaliacao-logica-js-miguel/src/ex2.js
@@ -0,0 +1,12 @@
+let n=7
+let soma=0
+
+
+
+for(let x=1;x<=10;x++){
+    let resultado = n * x
+    console.log(`${n} x ${x} = ${resultado}`)
+    soma = resultado
+}
+
+console.log(`soma dos resultados: ${soma}`)
